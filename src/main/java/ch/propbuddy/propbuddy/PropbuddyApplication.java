297286c22a97b26ef495e51a9108bf4439e9762c
Joel Jani <joel.luca.jani@gmail.com>
2020-04-13 18:17:14 +0200

    Project setup

---
+++ src/main/java/ch/propbuddy/propbuddy/PropbuddyApplication.java
@@ -0,0 +1,13 @@
+package ch.propbuddy.propbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/propbuddy/propbuddy/PropbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.propbuddy.propbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
